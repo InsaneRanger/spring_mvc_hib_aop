5c3cf27efe9fff485be699544852ace8991501b4
InsaneRanger <valeev.timur.v@gmail.com>
2020-12-10 17:32:46 +0300

    add dependency EmployeeDAO * @Autowired return getAllEmpl

--- src/main/java/com/spring/mvc_hibernate_aop/service/EmployeeServiceImpl.java
+++ src/main/java/com/spring/mvc_hibernate_aop/service/EmployeeServiceImpl.java
@@ -1,6 +1,8 @@
 package com.spring.mvc_hibernate_aop.service;
 
+import com.spring.mvc_hibernate_aop.dao.EmployeeDAO;
 import com.spring.mvc_hibernate_aop.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,8 +10,11 @@ import java.util.List;
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
     @Override
     public List<Employee> getAllEmployees() {
-        return null;
+        return employeeDAO.getAllEmployees();
     }
 }
