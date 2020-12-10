18de140d7c5fd7a5a6a80cd94684f6b56f61b2d1
InsaneRanger <valeev.timur.v@gmail.com>
2020-12-10 17:30:04 +0300

    implements methods in EmployeeServiceImpl.java (add class) add @Service annotation

---
+++ src/main/java/com/spring/mvc_hibernate_aop/service/EmployeeServiceImpl.java
@@ -0,0 +1,15 @@
+package com.spring.mvc_hibernate_aop.service;
+
+import com.spring.mvc_hibernate_aop.entity.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return null;
+    }
+}
